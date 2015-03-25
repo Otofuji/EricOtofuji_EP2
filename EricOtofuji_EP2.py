a5828703c001cedebd6d88402a82ab5f2f70ca37
Otofuji <ericfoa@al.insper.edu.br>
2015-03-25 11:39:40 -0300

    Added random

--- EricOtofuji_EP2.py
+++ EricOtofuji_EP2.py
@@ -6,3 +6,6 @@ Created on Wed Mar 25 10:29:02 2015
 """
 
 from turtle import *
+from random import *
+et = window.textinput("Forca da Força do Enforcamento","E a letra é... ") #et é a letra que o usuário escolhe e consequentemente digita para jogar este jogo
+db = open("db.txt", encoding="ansi") #db é o banco de dados de todas as palavras em língua portuguesa existentes, conforme alcor.concordia.ca/~vjorge/Palavras-Cruzadas/Lista-de-Palavras.txt

f9a45754b6dfa2757e04d5304aacb03c20066814
Otofuji <ericfoa@al.insper.edu.br>
2015-03-26 09:33:46 -0300

    NÃO, CRIATURA!
    Agora quando o usuário comete um erro, chamamos-no de criatura.
    Novidades desta versão também incluem:
    
    - substituição de random.choice por random.shuffle
    - possibilidade de input do usuário - apenas letras
    - caso input != letra, o programa xinga o usuário
    - todo usuário começa com 7 pontos
    - se letra != letra contida na palavra, -1 ponto
    - todo input é convertido para upper

--- EricOtofuji_EP2.py
+++ EricOtofuji_EP2.py
@@ -9,3 +9,17 @@ from turtle import *
 from random import *
 et = window.textinput("Forca da Força do Enforcamento","E a letra é... ") #et é a letra que o usuário escolhe e consequentemente digita para jogar este jogo
 db = open("db.txt", encoding="ansi") #db é o banco de dados de todas as palavras em língua portuguesa existentes, conforme alcor.concordia.ca/~vjorge/Palavras-Cruzadas/Lista-de-Palavras.txt
+w = random.shuffle(db.readline)
+while w == w:
+    w = random.shuffle(db.readline)
+l = input("Escolha uma letra: ")
+while l != a or b or c or d or e or f or g or h or i or j or k or l or m or n or o or p or q or r or s or t or u or v or w or x or y or z or A or B or C or D or E or F or G or H or I or J or K or L or M or N or O or P or Q or R or S or T or U or V or W or X or Y or Z:
+    l = input("NÃO, CRIATURA! Use apenas letras! Escolha uma LETRA: ")
+point = 7
+while point > 0:
+    l.upper = ans
+    if ans in w:
+        print(ans in w)
+    else:
+        point=-1
+        

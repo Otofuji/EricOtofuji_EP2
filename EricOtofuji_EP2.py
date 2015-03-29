d1ee38ff330cbcd3712a8b6fbcffd7803671bba3
Otofuji <ericfoa@al.insper.edu.br>
2015-03-29 20:28:45 -0300

    --

--- EricOtofuji_EP2.py
+++ EricOtofuji_EP2.py
@@ -5,8 +5,8 @@ Created on Wed Mar 25 10:29:02 2015
 @author: Eric Otofuji
 """
 
-from turtle import *
-from random import *
+import turtle
+import random
 et = window.textinput("Forca da Força do Enforcamento","E a letra é... ") #et é a letra que o usuário escolhe e consequentemente digita para jogar este jogo
 db = open("db.txt", encoding="ansi") #db é o banco de dados de todas as palavras em língua portuguesa existentes, conforme alcor.concordia.ca/~vjorge/Palavras-Cruzadas/Lista-de-Palavras.txt
 w = random.shuffle(db.readline)
@@ -22,4 +22,3 @@ while point > 0:
         print(ans in w)
     else:
         point=-1
-        

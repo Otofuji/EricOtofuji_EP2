e7d40b8f3e0e32143fbe3176e8cd620927a6e4c4
Otofuji <ericfoa@al.insper.edu.br>
2015-03-30 09:29:40 -0300

    Tentando resolver bugs e colocar as linhas das letrs

--- EricOtofuji_EP2.py
+++ EricOtofuji_EP2.py
@@ -7,7 +7,7 @@ Created on Wed Mar 25 10:29:02 2015
 
 import turtle
 import random
-window=turtle.screen()
+window=turtle.Screen()
 window.bgcolor("white")
 window.title("Forca da Força do Enforcamento")
 forca=turtle.Turtle()
@@ -27,8 +27,6 @@ forca.forward(dfdw)
 with open("db.txt", encoding="utf-8") as db: #from alcor.concordia.ca/~vjorge/Palavras-Cruzadas/Lista-de-Palavras.txt
     word=db.read().splitlines()
 w = random.shuffle(word)
-while w == w:
-    w = random.shuffle(word)
 error=0
 if error==1:
     err1=turtle.Turtle()
@@ -174,11 +172,17 @@ if error==7:
     err7.forward(dfdw)
     err7.forward(dfrg)
     print("Sinto muito. Você perdeu. É tarde demais e foi morto por enforcamento.")
+line=turtle.Turtle()
+line.setpos(400,85)
+line.penup()
+ang90=90
+line.right(ang90)
+len(w(line.write("___  ")))
 while error<7:
     et = input("E a letra é... ") #et é a letra que o usuário escolhe e consequentemente digita para jogar este jogo
-    while et != "a" or "b" or "c" or "d" or "e" or "f" or "g" or "h" or "i" or "j" or "k" or "l" or "m" or "n" or "o" or "p" or "q" or "r" or "s" or "t" or "u" or "v" or "w" or "x" or "y" or "z" or "A" or "B" or "C" or "D" or "E" or "F" or "G" or "H" or "I" or "J" or "K" or "L" or "M" or "N" or "O" or "P" or "Q" or "R" or "S" or "T" or "U" or "V" or "W" or "X" or "Y" or "Z" or "-":
-        et = input("NÃO, CRIATURA! Use apenas letras ou hífen! Escolha uma LETRA ou hífen: ")
-    if et in w:
-        print (et in w)
-    else:
-        error+=1
+    for i in w:    
+        if et in w:
+            print (et[i])
+        else:
+            error+=1
+window.exitonclick()
